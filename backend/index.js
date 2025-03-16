3eadeeaad7a07aa3158a5ef31bd0448aa60497ef
bhavyatiwari0987 <bhavyatiwari917@gmail.com>
2025-03-16 16:16:37 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -50,5 +50,9 @@ app.post("/auth/callback", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) =>{
+  res.send('<h1>Oauth</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
